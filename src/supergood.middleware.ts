2a8ad051768cdf2e2fb17c093a5463d2ab161c7a
Alex Klarfeld <alex.klarfeld@gmail.com>
2024-03-30 17:54:46 -0700

    Fix middleware

--- src/supergood.middleware.ts
+++ src/supergood.middleware.ts
@@ -1,22 +1,24 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import Supergood from 'supergood';
-
-Supergood.init({
-  clientId: process.env.SUPERGOOD_CLIENT_ID,
-  clientSecret: process.env.SUPERGOOD_CLIENT_SECRET,
-  config: {
-    useRemoteConfig: false,
-    useTelemetry: false,
-  },
-});
-
 @Injectable()
 export class SupergoodMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
-    res.on('finish', async () => {
-      await Supergood.waitAndFlushCache();
+
+    const supergood = require('Supergood');
+    Supergood.init({
+      clientId: process.env.SUPERGOOD_CLIENT_ID,
+      clientSecret: process.env.SUPERGOOD_CLIENT_SECRET,
+      config: {
+        useRemoteConfig: false,
+        useTelemetry: false,
+      },
     });
+
+    res.on('finish', async () => {
+      await Supergood.close();
+    });
+
     next();
   }
 }
