72b55f113b33c5fcbc6e1ff9da83b43d913281e6
Alex Klarfeld <1866143+aklarfeld@users.noreply.github.com>
2024-04-24 19:07:33 -0700

    Update supergood.middleware.ts

--- src/supergood.middleware.ts
+++ src/supergood.middleware.ts
@@ -1,20 +1,11 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import Supergood from 'supergood';
 
 @Injectable()
 export class SupergoodMiddleware implements NestMiddleware {
-
   async use(req: Request, res: Response, next: NextFunction) {
-    const Supergood = require('Supergood');
-    Supergood.init({
-      clientId: process.env.SUPERGOOD_CLIENT_ID,
-      clientSecret: process.env.SUPERGOOD_CLIENT_SECRET,
-      config: {
-        useRemoteConfig: false,
-        useTelemetry: false,
-      },
-    });
-
+    
     res.on('finish', async () => {
       await Supergood.close();
     });
